2b5c4f624e991a5ba9dd28e7a7f48eba159d8f4c
Zeddini Iheb <80251420+iheb147@users.noreply.github.com>
2026-05-25 05:12:08 -1200

    fix: Auto-correction via PoweredCodeReview

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 import time
 
 def process_logs(filename):
-    f = open(filename)
-    content = f.read()
+    with open(filename) as f:
+        content = f.read()
     errors = []
     for line in content.split("\n"):
         if "ERROR" in line:
